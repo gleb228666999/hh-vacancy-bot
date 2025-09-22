cf01401ecc7fa818d62bbb4f6f55956bb0855673
gleb228666999 <160999486+gleb228666999@users.noreply.github.com>
2025-09-22 19:05:20 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+# config.py
+
+# Токен вашего Telegram-бота
+TELEGRAM_TOKEN = "YOUR_TOKEN"
+
+# Заголовки для запросов
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"
+}
+
+# Город (area): 1 - Москва, 113 - Сургут (можно изменить)
+DEFAULT_AREA = 1
